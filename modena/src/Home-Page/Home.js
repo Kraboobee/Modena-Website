1805b7a2821cdba7f1727db9c44280955d83359c
kraboobee <kraboobee@gmail.com>
2022-07-18 22:57:38 +0200

    Create home page

---
+++ modena/src/Home-Page/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render(){
+        return (
+            <div>
+                <h1>TIRED OF SLOW COMPUTERS?</h1>
+                <h2>We're here to help</h2>
+                <p>
+                    We build high-end workstations tailored just for you
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Home;
