86e6e68cda239fb8121009c98450342921718c69
kraboobee <kraboobee@gmail.com>
2022-07-19 17:55:10 +0200

    Faded header image

--- modena/src/Home-Page/Home.js
+++ modena/src/Home-Page/Home.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import background from "./img/Header.jpg"
+import background from "./img/header-faded.jpeg"
 
 class Home extends Component {
     render(){
         return (
-            <div className="bg-cover h-screen flex items-center mb-5" style={{ backgroundImage: `url(${background})`}}>
+            <div className="bg-fixed bg-cover h-screen flex items-center mb-5" style={{ backgroundImage: `url(${background})`}}>
                 <div className="container mx-auto px-4 place-content-center">
                     <h1 className="text-center font-sans text-5xl text-white">TIRED OF SLOW COMPUTERS?</h1>
                     <h2 className="text-center font-sans text-5xl text-white">We're here to help</h2>
