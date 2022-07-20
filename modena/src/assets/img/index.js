fd28f65cf837c12cd4eac55e012828347e6a1da3
kraboobee <kraboobee@gmail.com>
2022-07-20 14:24:24 +0200

    stage stuff I forgot

---
+++ modena/src/assets/img/index.js
@@ -0,0 +1,8 @@
+export const Content = require('./industries/content.jpg');
+export const ContentHydra = require('./industries/content-h.png');
+export const Design = require('./industries/design.jpg');
+export const DesignHydra = require('./industries/design-h.png');
+export const Scientific = require('./industries/scientific.jpg');
+export const ScientificHydra = require('./industries/scientific-h.png');
+
+// export const 
