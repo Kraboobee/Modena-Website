09aa45b6a2cbd995fd4c81205e4e310eb2811703
kraboobee <kraboobee@gmail.com>
2022-07-20 14:53:19 +0200

    Add images

--- modena/src/assets/img/index.js
+++ modena/src/assets/img/index.js
@@ -5,4 +5,9 @@ export const DesignHydra = require('./industries/design-h.png');
 export const Scientific = require('./industries/scientific.jpg');
 export const ScientificHydra = require('./industries/scientific-h.png');
 
-// export const 
+export const NoImage = require('./software/cross.svg');
+export const Illustrator = require('./software/content/illustrator.svg');
+export const Incopy = require('./software/content/incopy.svg');
+export const Indesign = require('./software/content/indesign.svg');
+export const Lightroom = require('./software/content/lightroom-cc.svg');
+export const Photoshop = require('./software/content/photoshop.svg');
--- modena/src/homepage.js
+++ modena/src/homepage.js
@@ -1,4 +1,6 @@
 import { Content, ContentHydra, Design, DesignHydra, Scientific, ScientificHydra } from "./assets/img"
+import { NoImage } from "./assets/img"
+import { Illustrator, Incopy, Indesign, Lightroom, Photoshop } from "./assets/img"
 
 const solutionList = [
     {
@@ -24,7 +26,7 @@ const solutionList = [
 
 const softwareDefault = {
         industry:   null, // solutionList id -> 0: content creation
-        logo:       '', 
+        logo:       NoImage, 
         ram:        `The amount of memory you will need depends on how many programs you will have open at any given time alongside ${alias} and how large the files are that you will be working with. A good starting point in deciding how much RAM you should go for would be to check your current usage via Windows Task Manager.`, 
         ssd:        'In the past, computers were held back by slow mechanical hard drives.  Solid State drives should be used for the operating system and frequently accessed programs and files. Mechanical drives should only be used for extremely large files that are not used very often. Having everything stored on SSDs means you\’ll be able to copy, move, open and save files quickly and PC and program start times will be reduced.', 
         systems:    [1,1,0],
@@ -35,7 +37,7 @@ const contentSoftwareList = [
         industry:   0, // solutionList id -> 0: content creation
         name:       'Adobe After Effects', 
         alias:      'After Effects',
-        logo:       '', 
+        logo:       NoImage, 
         cpu:        '',
         ram:        softwareDefault.ram, 
         gpu:        '', 
@@ -46,10 +48,10 @@ const contentSoftwareList = [
         industry:   0, // solutionList id -> 0: content creation
         name:       'Adobe Illustrator', 
         alias:      'Illustrator',
-        logo:       '', 
+        logo:       Illustrator, 
         cpu:        'Adobe Illustrator relies entirely on CPU speed. Frequency is measured in “GHz”, and this is often listed in the specs. We would recommend opting for the fastest CPU possible. Not all user workflows are the same though, so be sure and contact us for an optimized configuration.',
         ram:        softwareDefault.ram, 
-        gpu:        'Adobe Illustrator does not require a powerful graphics card, but certain features can make use of GPU power to a certain extent. A dedicated GPU provides a much smoother experience when panning and zooming \– it also lets you zoom in much closer. A mid-range card would offer the best value. As always, you should keep in mind other software you use that could rely more on GPU power. \nAdobe does not require a workstation grade GPU (Nvidia Quadro or AMD Radeon Pro), and we would recommend a Nvidia GeForce card for optimal performance, stability and value.', 
+        gpu:        'Adobe Illustrator does not require a powerful graphics card, but certain features can make use of GPU power to a certain extent. A dedicated GPU provides a much smoother experience when panning and zooming – it also lets you zoom in much closer. A mid-range card would offer the best value. As always, you should keep in mind other software you use that could rely more on GPU power. \nAdobe does not require a workstation grade GPU (Nvidia Quadro or AMD Radeon Pro), and we would recommend a Nvidia GeForce card for optimal performance, stability and value.', 
         ssd:        softwareDefault.ssd, 
         systems:    softwareDefault.systems,
     },
@@ -57,10 +59,10 @@ const contentSoftwareList = [
         industry:   0, // solutionList id -> 0: content creation
         name:       'Adobe Incopy', 
         alias:      'Incopy',
-        logo:       '', 
-        cpu:        '',
+        logo:       Incopy, 
+        cpu:        'Adobe Incopy relies primarily on CPU speed, but it doesn\’t require an especially powerful CPU. Frequency is measured in “GHz”, and this is often listed in the specs. We would recommend opting for the fastest CPU within your budget. Not all user workflows are the same though, so be sure and contact us for an optimized configuration.',
         ram:        softwareDefault.ram, 
-        gpu:        '', 
+        gpu:        'Adobe Incopy does not require a powerful graphics card, but some features can make use of GPU power to a certain extent. A dedicated GPU provides a much smoother experience when panning and zooming – it also lets you zoom in much closer. A mid-range card would offer the best value. As always, you should keep in mind other software you use that could rely more on GPU power. \nAdobe does not require a workstation grade GPU (Nvidia Quadro or AMD Radeon Pro), and we would recommend a Nvidia GeForce card for optimal performance, stability and value.', 
         ssd:        softwareDefault.ssd, 
         systems:    softwareDefault.systems,
     },
@@ -68,10 +70,10 @@ const contentSoftwareList = [
         industry:   0, // solutionList id -> 0: content creation
         name:       'Adobe Indesign', 
         alias:      'Indesign',
-        logo:       '', 
-        cpu:        '',
+        logo:       Indesign, 
+        cpu:        'Adobe Indesign relies primarily on CPU speed, but it doesn\’t require an especially powerful CPU. Frequency is measured in “GHz”, and this is often listed in the specs. We would recommend opting for the fastest CPU within your budget. Not all user workflows are the same though, so be sure and contact us for an optimized configuration.',
         ram:        softwareDefault.ram, 
-        gpu:        '', 
+        gpu:        'Adobe Indesign does not require a powerful graphics card, but some features can make use of GPU power to a certain extent. A dedicated GPU provides a much smoother experience when panning and zooming – it also lets you zoom in much closer. A mid-range card would offer the best value. As always, you should keep in mind other software you use that could rely more on GPU power. \nAdobe does not require a workstation grade GPU (Nvidia Quadro or AMD Radeon Pro), and we would recommend a Nvidia GeForce card for optimal performance, stability and value.', 
         ssd:        softwareDefault.ssd, 
         systems:    softwareDefault.systems,
     },
@@ -79,10 +81,10 @@ const contentSoftwareList = [
         industry:   0, // solutionList id -> 0: content creation
         name:       'Adobe Lightroom', 
         alias:      'Lightroom',
-        logo:       '', 
-        cpu:        '',
+        logo:       Lightroom, 
+        cpu:        'Adobe Lightroom relies mostly on CPU speed (GHz), but many processes in Photoshop take advantage of multiple CPU cores up to a certain point. These processes include applying blur effects and adjusting colour and contrast. We would recommend opting for the fastest CPU possible that still has a reasonable core count. Not all user workflows are the same though, so be sure and contact us for an optimized configuration.',
         ram:        softwareDefault.ram, 
-        gpu:        '', 
+        gpu:        'Lightroom does not require a powerful graphics card, but certain features can make use of GPU power to a certain extent. A dedicated GPU provides a much smoother experience when panning and zooming – it also lets you zoom in much closer. A mid-range card would offer the best value. As always, you should keep in mind other software you use that could rely more on GPU power. \nAdobe does not require a workstation grade GPU (Nvidia Quadro or AMD Radeon Pro), and we would recommend a Nvidia GeForce card for optimal performance, stability and value.', 
         ssd:        softwareDefault.ssd, 
         systems:    softwareDefault.systems,
     },
@@ -90,10 +92,10 @@ const contentSoftwareList = [
         industry:   0, // solutionList id -> 0: content creation
         name:       'Adobe Photoshop', 
         alias:      'Photoshop',
-        logo:       '', 
-        cpu:        '',
+        logo:       Photoshop, 
+        cpu:        'Adobe Photoshop relies mostly on CPU speed (GHz), but many processes in Photoshop take advantage of multiple CPU cores up to a certain point. These processes include applying blur effects and adjusting colour and contrast. We would recommend opting for the fastest CPU possible that still has a reasonable core count. Not all user workflows are the same though, so be sure and contact us for an optimized configuration.',
         ram:        softwareDefault.ram, 
-        gpu:        '', 
+        gpu:        'Photoshop does not require a powerful graphics card, but certain features can make use of GPU power to a certain extent. A dedicated GPU provides a much smoother experience when panning and zooming – it also lets you zoom in much closer. A mid-range card would offer the best value. As always, you should keep in mind other software you use that could rely more on GPU power. \nAdobe does not require a workstation grade GPU (Nvidia Quadro or AMD Radeon Pro), and we would recommend a Nvidia GeForce card for optimal performance, stability and value.', 
         ssd:        softwareDefault.ssd, 
         systems:    softwareDefault.systems,
     },
@@ -101,10 +103,10 @@ const contentSoftwareList = [
         industry:   0, // solutionList id -> 0: content creation
         name:       'Adobe Premiere Pro', 
         alias:      'Premiere Pro',
-        logo:       '', 
-        cpu:        '',
+        logo:       NoImage, 
+        cpu:        'Adobe Premiere Pro can make use of multiple CPU cores, but the efficiency greatly drops after a certain point. For most users, we would recommend opting for the fastest CPU available that still has a decent core count. Not all user workflows are the same though, so be sure and contact us for an optimized configuration.',
         ram:        softwareDefault.ram, 
-        gpu:        '', 
+        gpu:        'Your graphics card can make a big difference for Premiere Pro. A mid-range card will be suitable for 1080p editing, but you\’ll want something with more VRAM for editing at higher resolutions. Exporting to H.264 also requires more GPU power. \nWhile Premiere Pro does support multiple GPUs, this feature does not improve performance over a single card by much.  \nAdobe does not require a workstation grade GPU (Nvidia Quadro or AMD Radeon Pro), and we would recommend a Nvidia GeForce card for the greatest value.', 
         ssd:        softwareDefault.ssd, 
         systems:    softwareDefault.systems,
     },
@@ -115,7 +117,7 @@ const engineeringSoftwareList = [
         industry:   1, // solutionList id -> 0: content creation
         name:       '', 
         alias:      '',
-        logo:       '', 
+        logo:       NoImage, 
         cpu:        '',
         ram:        softwareDefault.ram,
         gpu:        '', 
@@ -129,7 +131,7 @@ const scientificSoftwareList = [
         industry:   2, // solutionList id -> 0: content creation
         name:       '', 
         alias:      '',
-        logo:       '', 
+        logo:       NoImage, 
         cpu:        '',
         ram:        softwareDefault.ram,
         gpu:        '', 
@@ -138,4 +140,4 @@ const scientificSoftwareList = [
     },
 ]
 
-export {solutionList}
+export {solutionList, contentSoftwareList, engineeringSoftwareList, scientificSoftwareList}
